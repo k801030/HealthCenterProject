efd42451b49dc8c49ca65c62ce2d3ba819836c3c
Vison <kk801030@gmail.com>
2014-12-26 17:58:33 +0800

    nav auto active

---
+++ js/basic.js
@@ -0,0 +1,13 @@
+angular.module("basic", [])
+
+.controller('NavController', ['$scope', '$location' , function($scope, $location){
+  $scope.navUrl = 'ajax/nav.html';
+  $scope.isActive = function (viewLocation) {
+    // get the last part of url 
+    // and return true/false 
+    var urlArray = $location.absUrl().split("/");
+    var segment = urlArray[urlArray.length-1]; 
+    return viewLocation === segment;
+  };
+  $scope.location = $location.path();
+}])
