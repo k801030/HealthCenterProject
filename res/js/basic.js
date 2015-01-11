7dfe3565c91483d506a7ef5017b5af169dc8b67f
Vison <kk801030@gmail.com>
2015-01-11 14:51:52 +0800

    nav dropdown on hover

---
+++ res/js/basic.js
@@ -0,0 +1,36 @@
+angular.module("basic", ['ui.bootstrap'])
+
+.controller('NavController', ['$scope', '$location', '$http', function($scope, $location, $http){
+  $scope.navUrl = 'res/ajax/nav.html';
+  $scope.navItems;
+  $http.get('res/nav.json')
+    .then(function(res){
+      $scope.navItems = res.data;
+    });
+  $scope.isActive = function (viewLocation) {
+    // get the last part of url 
+    // and return true/false 
+    var urlArray = $location.absUrl().split("/");
+    var segment = urlArray[urlArray.length-1]; 
+    return viewLocation === segment;
+  };
+  $scope.location = $location.path();
+
+
+}])
+
+.controller('SideBarController', ['$scope', function($scope){
+  
+}])
+
+.controller('DropdownCtrl', ['$scope', '$log', function($scope, $log) {
+
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+  };
+}])
