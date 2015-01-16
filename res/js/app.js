7c8059dbfa00d1c60ef1ba938c1cf6d317c266f5
Vison <kk801030@gmail.com>
2015-01-16 16:13:30 +0800

    nav-fixed-top bar

--- res/js/app.js
+++ res/js/app.js
@@ -1,3 +1,26 @@
+// angularJS
 // include your js module or put it on depend on usage.
 angular.module('app', ['basic', 'client'])  
+
+// javascript & jQuery
+$(document).ready(function() {
+
+  console.log($( window ).height());
+   
+  
+  // Returns height of HTML document
+  $(window).scroll(function() {
+    console.log($('nav').height());
+    if($( document ).scrollTop() >= 45) {
+      //$('#content').addClass('is-fixed-top');
+      //$('#main-nav').addClass('nav-fixed-top');
+      $('#main-nav').addClass('skinny');
+    } else {
+      //$('#content').removeClass('is-fixed-top');
+      //$('#main-nav').removeClass('nav-fixed-top');
+      $('#main-nav').removeClass('skinny');
+    }
+  });
+
+});
 
