bc554a3812acee4e9ba4d3c2fcf80409b72ea985
pilagod <r03725029@ntu.edu.tw>
2015-03-09 01:46:58 +0800

    health_info

--- res/js/pages.js
+++ res/js/pages.js
@@ -8,43 +8,55 @@ angular.module("pages", [])
     Tabletop.init({
         key: '1t5_YYxLzpunJ7gw_ghlubsnGtIw-b5Evh2MITmn67Wk',
         callback: function(data, tabletop) {
+            console.log("test");
+
+            var sheet;
+
             for(var sheet_index in tabletop.sheets()){
                 console.log(tabletop.sheets(sheet_index));
-                if(sheet_index == 'history' || sheet_index == 'feature'){
-                    for(var element_index in tabletop.sheets(sheet_index).elements){
-                        if(tabletop.sheets(sheet_index).elements[element_index].content !== "")
-                            tabletop.sheets(sheet_index).elements[element_index].content =
-                                tabletop.sheets(sheet_index).elements[element_index].content.split('\n');
+
+                sheet = tabletop.sheets(sheet_index);
+
+                if(sheet_index == 'introduction_history' || sheet_index == 'introduction_feature'){
+                    for(var element_index in sheet.elements){
+                        if(sheet.elements[element_index].content !== "")
+                            sheet.elements[element_index].content =
+                                sheet.elements[element_index].content.split('\n');
                     }
                 }
-                else if(sheet_index == 'dr'){
-                    for(var element_index in tabletop.sheets(sheet_index).elements){
-                        tabletop.sheets(sheet_index).elements[element_index].title =
-                            tabletop.sheets(sheet_index).elements[element_index].title.split('\n');
+                else if(sheet_index == 'cast_dr'){
+                    for(var element_index in sheet.elements){
+                        sheet.elements[element_index].title =
+                            sheet.elements[element_index].title.split('\n');
 
-                        tabletop.sheets(sheet_index).elements[element_index].education =
-                            tabletop.sheets(sheet_index).elements[element_index].education.split('\n');
+                        sheet.elements[element_index].education =
+                            sheet.elements[element_index].education.split('\n');
 
-                        tabletop.sheets(sheet_index).elements[element_index].now_at =
-                            tabletop.sheets(sheet_index).elements[element_index].now_at.split('\n');
+                        sheet.elements[element_index].now_at =
+                            sheet.elements[element_index].now_at.split('\n');
 
-                        tabletop.sheets(sheet_index).elements[element_index].experience =
-                            tabletop.sheets(sheet_index).elements[element_index].experience.split('\n');
+                        sheet.elements[element_index].experience =
+                            sheet.elements[element_index].experience.split('\n');
 
-                        tabletop.sheets(sheet_index).elements[element_index].specialist =
-                            tabletop.sheets(sheet_index).elements[element_index].specialist.split('\n');
+                        sheet.elements[element_index].specialist =
+                            sheet.elements[element_index].specialist.split('\n');
                     }
                 }
-                else if(sheet_index == 'health_info'){
-                    for(var element_index in tabletop.sheets(sheet_index).elements){
-                        for(var i = 1; i <= 10; i++){
-                            tabletop.sheets(sheet_index).elements[element_index]["content" + i] =
-                                tabletop.sheets(sheet_index).elements[element_index]["content" + i].split('\n');
+                else if(sheet_index == 'health_know_healthcare' || sheet_index == 'health_info_disease' || sheet_index == 'health_info_QA'){
+                    for(var element_index in sheet.elements){
+                        for(var i = 1; i <= 20; i++){
+                            sheet.elements[element_index]["content" + i] =
+                                sheet.elements[element_index]["content" + i].split('\n');
                         }
-                        tabletop.sheets(sheet_index).elements[element_index].content =
-                            tabletop.sheets(sheet_index).elements[element_index].content.split('\n');
+                        sheet.elements[element_index].content =
+                            sheet.elements[element_index].content.split('\n');
+                        sheet.elements[element_index].footer =
+                            sheet.elements[element_index].footer.split('\n');
                     }
                     console.log(tabletop.sheets(sheet_index));
+                }
+                else if(sheet_index == 'home_slider'){
+
                 }
 
             }
@@ -98,9 +110,9 @@ angular.module("pages", [])
 
 .controller('history', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
     getSpreadSheetData.then(function(tabletop){
-        var history_data = tabletop.sheets('history');
+        var history_data = tabletop.sheets('introduction_history');
         scopeService.safeApply($scope, function(){
-                console.log(tabletop.sheets('history'));
+                console.log(tabletop.sheets('introduction_history'));
                 console.log(history_data.elements);
                 $scope.header = history_data.elements[1];
                 $scope.contents = history_data.elements.slice(2);
@@ -111,9 +123,9 @@ angular.module("pages", [])
 
 .controller('feature', ['$scope', 'getSpreadSheetData', 'scopeService',function($scope, getSpreadSheetData, scopeService){
     getSpreadSheetData.then(function(tabletop){
-        var feature_data = tabletop.sheets('feature');
+        var feature_data = tabletop.sheets('introduction_feature');
         scopeService.safeApply($scope, function(){
-                console.log(tabletop.sheets('feature'));
+                console.log(tabletop.sheets('introduction_feature'));
                 console.log(feature_data.elements);
                 $scope.header = feature_data.elements[1];
                 $scope.contents = feature_data.elements.slice(2);
@@ -121,10 +133,6 @@ angular.module("pages", [])
         );
     });
 
-}])
-
-.controller('FQA', ['$scope', function($scope){
-    wrapSetting();
 }])
 
 .controller('government_employee', ['$scope', function($scope){
@@ -170,9 +178,9 @@ angular.module("pages", [])
 
 .controller('dr', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
     getSpreadSheetData.then(function(tabletop){
-        var dr_data = tabletop.sheets('dr');
+        var dr_data = tabletop.sheets('cast_dr');
         scopeService.safeApply($scope, function(){
-                console.log(tabletop.sheets('feature'));
+                console.log(tabletop.sheets('cast_dr'));
                 console.log(dr_data.elements);
                 $scope.header = dr_data.elements[1];
                 $scope.contents = dr_data.elements.slice(2);
@@ -181,11 +189,42 @@ angular.module("pages", [])
     });
 }])
 
+.controller('FQA', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
+//    wrapSetting();
+    getSpreadSheetData.then(function(tabletop){
+        var health_info_QA_data = tabletop.sheets('health_info_QA');
+        scopeService.safeApply($scope, function(){
+                console.log(tabletop.sheets('health_info_QA'));
+                console.log(health_info_QA_data.elements);
+                $scope.header = health_info_QA_data.elements[1];
+                $scope.contents = health_info_QA_data.elements.slice(2);
+            }
+        );
+    });
+
+    $scope.showContent = showContent;
+}])
+
+.controller('reminder', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
+    getSpreadSheetData.then(function(tabletop){
+        var health_know_data = tabletop.sheets('health_know_healthcare');
+        scopeService.safeApply($scope, function(){
+                console.log(tabletop.sheets('health_know_healthcare'));
+                console.log(health_know_data.elements);
+                $scope.header = health_know_data.elements[1];
+                $scope.contents = health_know_data.elements.slice(2);
+            }
+        );
+    });
+
+    $scope.showContent = showContent;
+}])
+
 .controller('health_info', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
     getSpreadSheetData.then(function(tabletop){
-        var health_info_data = tabletop.sheets('health_info');
+        var health_info_data = tabletop.sheets('health_info_disease');
         scopeService.safeApply($scope, function(){
-                console.log(tabletop.sheets('feature'));
+                console.log(tabletop.sheets('health_info_disease'));
                 console.log(health_info_data.elements);
                 $scope.header = health_info_data.elements[1];
                 $scope.contents = health_info_data.elements.slice(2);
@@ -193,20 +232,50 @@ angular.module("pages", [])
         );
     });
 
-    $scope.showContent = function(e){
-        var $this = $(e.currentTarget);
-        if(!$this.hasClass("li_on_target")){
-            $this.addClass("li_on_target");
-            $this.children("label:eq(0)").addClass("title_on_target");
-            $this.children("div[name='wrap_content']:eq(0)").addClass("content_on_target");
-        }
-        else{
-            $this.removeClass("li_on_target");
-            $this.children("label:eq(0)").removeClass("title_on_target");
-            $this.children("div[name='wrap_content']:eq(0)").removeClass("content_on_target");
-        }
-    }
+    $scope.showContent = showContent;
+}])
 
+
+
+// Controller  for Carousel
+//.controller('CarouselCtrl', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService) {
+//// initializing the time Interval
+//    $scope.myInterval = 5000;
+//
+//    getSpreadSheetData.then(function(tabletop){
+//        var home_slider_data = tabletop.sheets('home_slider');
+//        scopeService.safeApply($scope, function(){
+//                console.log(tabletop.sheets('home_slider'));
+//                console.log(home_slider_data.elements);
+//                $scope.slides = home_slider_data.elements.slice(1);
+//            }
+//        );
+//    });
+//
+////    // Initializing  slide rray
+////    $scope.slides = [
+////        {image:'res/images/sliders/1.jpg',title:'id1', text:'馬偕醫院淡水分區，期望帶給大家健康溫馨的生活。'},
+////        {image:'res/images/sliders/2.jpg',title:'', text:'夏天、海洋。'},
+////        {image:'res/images/sliders/3.jpg',title:'', text:'自然。'}];
+//
+////    var slides = $scope.slides;
+//
+//}])
+
+.controller('home', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService) {
+    $scope.myInterval = 5000;
+
+    getSpreadSheetData.then(function(tabletop){
+        var home_slider_data = tabletop.sheets('home_slider');
+        var home_info_data = tabletop.sheets('home_info');
+        scopeService.safeApply($scope, function(){
+                console.log(tabletop.sheets('home_slider'));
+                console.log(home_slider_data.elements);
+                $scope.slides = home_slider_data.elements.slice(1);
+                $scope.infos = home_info_data.elements.slice(1);
+            }
+        );
+    });
 }]);
 
 
@@ -255,4 +324,18 @@ function wrapSetting(){
             $(this).children("div[name='wrap_content']:eq(0)").removeClass("content_on_target");
         }
     });
+}
+
+function showContent(e){
+    var $this = $(e.currentTarget);
+    if(!$this.hasClass("li_on_target")){
+        $this.addClass("li_on_target");
+        $this.children("label:eq(0)").addClass("title_on_target");
+        $this.children("div[name='wrap_content']:eq(0)").addClass("content_on_target");
+    }
+    else{
+        $this.removeClass("li_on_target");
+        $this.children("label:eq(0)").removeClass("title_on_target");
+        $this.children("div[name='wrap_content']:eq(0)").removeClass("content_on_target");
+    }
 }
