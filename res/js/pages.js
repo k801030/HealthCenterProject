6a63b376daedfa4b75c2267aee7ce99fe75b6cd9
pilagod <r03725029@ntu.edu.tw>
2015-03-14 14:14:06 +0800

    finish

--- res/js/pages.js
+++ res/js/pages.js
@@ -8,7 +8,6 @@ angular.module("pages", [])
     Tabletop.init({
         key: '1t5_YYxLzpunJ7gw_ghlubsnGtIw-b5Evh2MITmn67Wk',
         callback: function(data, tabletop) {
-
             var sheet;
 
             for(var sheet_index in tabletop.sheets()){
@@ -16,15 +15,19 @@ angular.module("pages", [])
 
                 sheet = tabletop.sheets(sheet_index);
 
-                if(sheet_index == 'introduction_history' || sheet_index == 'introduction_feature'){
-                    for(var element_index in sheet.elements){
+                if(sheet_index == 'introduction_history' || sheet_index == 'introduction_feature' || sheet_index == 'introduction_environment' ||
+                   sheet_index == 'cast_equipment' || sheet_index == 'cast_team'){
+                    for(var element_index in sheet.elements.slice(1)){
+                        element_index = parseInt(element_index) + 1;
                         if(sheet.elements[element_index].content !== "")
                             sheet.elements[element_index].content =
                                 sheet.elements[element_index].content.split('\n');
                     }
                 }
                 else if(sheet_index == 'cast_dr'){
-                    for(var element_index in sheet.elements){
+                    for(var element_index in sheet.elements.slice(1)){
+                        element_index = parseInt(element_index) + 1;
+
                         sheet.elements[element_index].title =
                             sheet.elements[element_index].title.split('\n');
 
@@ -39,32 +42,88 @@ angular.module("pages", [])
 
                         sheet.elements[element_index].specialist =
                             sheet.elements[element_index].specialist.split('\n');
+
                     }
                 }
-                else if(sheet_index == 'item_oneday' || sheet_index == 'item_work' || sheet_index == 'item_foreign' || sheet_index == 'item_adult' ||
-                        sheet_index == 'health_know_healthcare' || sheet_index == 'health_info_disease' || sheet_index == 'health_info_QA'){
-                    for(var element_index in sheet.elements){
+                else if(sheet_index == 'item_oneday' || sheet_index == 'item_work' || sheet_index == 'item_government' ||
+                        sheet_index == 'item_foreign' || sheet_index == 'item_adult' || sheet_index == 'item_event'){
+                    for(var element_index in sheet.elements.slice(2)){
+                        element_index = parseInt(element_index) + 2;
                         for(var i = 1; i <= 20; i++){
-                            sheet.elements[element_index]["content" + i] =
-                                sheet.elements[element_index]["content" + i].split('\n');
+                            if(sheet.elements[element_index]["content" + i] !== ''){
+                                sheet.elements[element_index]["content" + i] =
+                                    sheet.elements[element_index]["content" + i].split('\n');
+                            }
                         }
-                        sheet.elements[element_index].content =
-                            sheet.elements[element_index].content.split('\n');
-                        sheet.elements[element_index].footer =
-                            sheet.elements[element_index].footer.split('\n');
+                        if(sheet.elements[element_index].content !== ''){
+                            sheet.elements[element_index].content =
+                                sheet.elements[element_index].content.split('\n');
+                        }
                     }
                     //console.log(tabletop.sheets(sheet_index));
                 }
-                else if(sheet_index == 'home_slider'){
-
+                else if(sheet_index == 'health_know_healthcare' || sheet_index == 'health_info_disease' || sheet_index == 'health_info_QA' ||
+                        sheet_index == 'health_info_nutrition' || sheet_index == 'health_info_nursing' || sheet_index == 'health_info_news'){
+                    for(var element_index in sheet.elements.slice(2)){
+                        element_index = parseInt(element_index) + 2;
+                        for(var i = 1; i <= 20; i++){
+                            if(sheet.elements[element_index]["content" + i] !== '') {
+                                sheet.elements[element_index]["content" + i] =
+                                    sheet.elements[element_index]["content" + i].split('\n');
+                            }
+                        }
+                        if(sheet.elements[element_index].content !== '') {
+                            sheet.elements[element_index].content =
+                                sheet.elements[element_index].content.split('\n');
+                        }
+                        if(sheet.elements[element_index].footer !== '') {
+                            sheet.elements[element_index].footer =
+                                sheet.elements[element_index].footer.split('\n');
+                        }
+                    }
                 }
-
             }
             deferred.resolve(tabletop);
         }
     });
     return deferred.promise;
 })
+
+//.service('getHomeSpreadSheetData', ['$q', 'getSpreadSheetData', function($q, getSpreadSheetData) {
+//
+//    var deferred = $q.defer();
+//    getSpreadSheetData.then(function(tabletop){
+//        var data = [];
+//        data.push(tabletop.sheets('home_slider'));
+//        data.push(tabletop.sheets('home_info'));
+//
+//
+//
+//        deferred.resolve(data);
+//    });
+//    return deferred.promise;
+//}])
+
+//.service('getIntroductionSpreadSheetData', ['$q', 'getSpreadSheetData', function($q, getSpreadSheetData) {
+//
+//    var deferred = $q.defer();
+//    getSpreadSheetData.then(function(tabletop){
+//        var data = [];
+//        data.push(tabletop.sheets('introduction_history'));
+//        data.push(tabletop.sheets('introduction_feature'));
+//        data.push(tabletop.sheets('introduction_environment'));
+//
+//        for(var index in data) {
+//            console.log(index);
+////            if (sheet.elements[element_index].content !== "") {
+////                sheet.elements[element_index].content =
+////                    sheet.elements[element_index].content.split('\n');
+////            }
+//        }
+//        deferred.resolve(data);
+//    });
+//    return deferred.promise;
+//}])
 
 .service('scopeService', function() {
     return {
@@ -135,6 +194,19 @@ angular.module("pages", [])
 
 }])
 
+.controller('environment', ['$scope', 'getSpreadSheetData', 'scopeService',function($scope, getSpreadSheetData, scopeService){
+    getSpreadSheetData.then(function(tabletop){
+        var environment_data = tabletop.sheets('introduction_environment');
+        scopeService.safeApply($scope, function(){
+                //console.log(tabletop.sheets('introduction_feature'));
+                //console.log(feature_data.elements);
+                $scope.header = environment_data.elements[1];
+                $scope.contents = environment_data.elements.slice(2);
+            }
+        );
+    });
+}])
+
 .controller('government_employee', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService) {
 //    $scope.tab_header_index = 0;
     $scope.tab_section_index = 1;
@@ -198,9 +270,10 @@ angular.module("pages", [])
 
     getSpreadSheetData.then(function(tabletop){
         var item_oneday_data = tabletop.sheets('item_oneday');
+        $scope.link = "";
         scopeService.safeApply($scope, function(){
-                //console.log(tabletop.sheets('item_oneday'));
-                //console.log(item_oneday_data.elements);
+//                console.log(tabletop.sheets('item_oneday'));
+//                console.log(item_oneday_data.elements);
                 $scope.header = item_oneday_data.elements[1];
                 $scope.contents = item_oneday_data.elements.slice(2);
             }
@@ -256,6 +329,48 @@ angular.module("pages", [])
         );
     });
 }])
+
+
+.controller('event', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService) {
+    getSpreadSheetData.then(function(tabletop){
+        var item_event_data = tabletop.sheets('item_event');
+        scopeService.safeApply($scope, function(){
+                //console.log(tabletop.sheets('item_adult'));
+                //console.log(item_adult_data.elements);
+                $scope.header = item_event_data.elements[1];
+                $scope.contents = item_event_data.elements.slice(2);
+            }
+        );
+    });
+}])
+
+.controller('equipment', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
+    getSpreadSheetData.then(function(tabletop){
+        var cast_equipment_data = tabletop.sheets('cast_equipment');
+        scopeService.safeApply($scope, function(){
+                //console.log(tabletop.sheets('introduction_history'));
+                //console.log(history_data.elements);
+                $scope.header = cast_equipment_data.elements[1];
+                $scope.contents = cast_equipment_data.elements.slice(2);
+            }
+        );
+    });
+}])
+
+
+.controller('team', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
+    getSpreadSheetData.then(function(tabletop){
+        var cast_team_data = tabletop.sheets('cast_team');
+        scopeService.safeApply($scope, function(){
+                //console.log(tabletop.sheets('introduction_history'));
+                //console.log(history_data.elements);
+                $scope.header = cast_team_data.elements[1];
+                $scope.contents = cast_team_data.elements.slice(2);
+            }
+        );
+    });
+}])
+
 
 .controller('dr', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
     getSpreadSheetData.then(function(tabletop){
@@ -316,6 +431,53 @@ angular.module("pages", [])
     $scope.showContent = showContent;
 }])
 
+.controller('nutrition_consultation', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
+    getSpreadSheetData.then(function(tabletop){
+        var nutrition_consultation_data = tabletop.sheets('health_info_nutrition');
+        scopeService.safeApply($scope, function(){
+                //console.log(tabletop.sheets('health_info_disease'));
+                //console.log(health_info_data.elements);
+                $scope.header = nutrition_consultation_data.elements[1];
+                $scope.contents = nutrition_consultation_data.elements.slice(2);
+            }
+        );
+    });
+
+    $scope.showContent = showContent;
+}])
+
+
+.controller('nursing_consultation', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
+    getSpreadSheetData.then(function(tabletop){
+        var nursing_consultation_data = tabletop.sheets('health_info_nursing');
+        scopeService.safeApply($scope, function(){
+                //console.log(tabletop.sheets('health_info_disease'));
+                //console.log(health_info_data.elements);
+                $scope.header = nursing_consultation_data.elements[1];
+                $scope.contents = nursing_consultation_data.elements.slice(2);
+            }
+        );
+    });
+
+    $scope.showContent = showContent;
+}])
+
+.controller('medical_info', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService){
+    getSpreadSheetData.then(function(tabletop){
+        var medical_info_data = tabletop.sheets('health_info_news');
+        scopeService.safeApply($scope, function(){
+                //console.log(tabletop.sheets('health_info_disease'));
+                //console.log(health_info_data.elements);
+                $scope.header = medical_info_data.elements[1];
+                $scope.contents = medical_info_data.elements.slice(2);
+            }
+        );
+    });
+
+    $scope.showContent = showContent;
+}])
+
+
 
 
 // Controller  for Carousel
@@ -345,10 +507,11 @@ angular.module("pages", [])
 
 .controller('home', ['$scope', 'getSpreadSheetData', 'scopeService', function($scope, getSpreadSheetData, scopeService) {
     $scope.myInterval = 5000;
-
     getSpreadSheetData.then(function(tabletop){
         var home_slider_data = tabletop.sheets('home_slider');
         var home_info_data = tabletop.sheets('home_info');
+//        var home_slider_data = data[0]; //home_slider
+//        var home_info_data = data[1]; //home_info
         scopeService.safeApply($scope, function(){
                 //console.log(tabletop.sheets('home_slider'));
                 //console.log(home_slider_data.elements);
